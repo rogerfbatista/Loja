24a437f49960364ed00b6eaa0f326d4cf2a20f24
rogerfbatista <rogerfbatista@hotmail.com>
2017-12-14 11:18:29 -0200

    add Handler

---
+++ WebApplication1/WebApplication1/Handler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Descrição resumida de Handler
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "texto/simples";
+            context.Response.Write("Olá, Mundo");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
